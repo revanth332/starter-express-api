3001dd726cfbe38627ae6e7ed63a56343d75a6bd
revanth332 <94832310+revanth332@users.noreply.github.com>
2024-06-30 13:37:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,11 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.get("/test",(req,res) => {
+    res.send({
+        name : "revanth",
+        age : 22
+    })
+})
 app.listen(process.env.PORT || 3000)
